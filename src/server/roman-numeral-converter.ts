0632b5ccf662143594da2f1997f8eb41bdb93415
cliff76 <270462+cliff76@users.noreply.github.com>
2025-06-04 14:05:13 -0700

    Implement edge case error handling.

--- src/server/roman-numeral-converter.ts
+++ src/server/roman-numeral-converter.ts
@@ -1,5 +1,13 @@
 export default class RomanNumeralConverter {
     static convert(num: number): string {
+        if (!Number.isInteger(num)) {
+            throw new Error('Input must be an integer.');
+        }
+
+        if (num < 0 || num > 3999) {
+            throw new Error('Input number out of range (must be 0-3999).');
+        }
+
         const romanSymbols: [string, number][] = [
             ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400], ['C', 100],
             ['XC', 90], ['L', 50], ['XL', 40], ['X', 10], ['IX', 9],
