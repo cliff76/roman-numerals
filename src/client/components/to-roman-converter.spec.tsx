4cc8d3a6bc3005e1e4b3888c3941c037a1f89ef9
Clifton <270462+cliff76@users.noreply.github.com>
2025-06-05 05:46:20 -0700

    Localizes server error messages. (#21)
    (cherry picked from commit ff23896a71b0bbd4d795c5f1754348180609c3f3)

--- src/client/components/to-roman-converter.spec.tsx
+++ src/client/components/to-roman-converter.spec.tsx
@@ -218,7 +218,7 @@ describe('ToRomanConverter behaviors', () => {
       mutate: mutateMock,
       isPending: false,
       isError: true,
-      error: new Error(errorMessage),
+      error: {response: {data: {error: errorMessage}}},
     } as any)
     renderComponent();
     expect(screen.getByText(errorMessage)).toBeInTheDocument();
